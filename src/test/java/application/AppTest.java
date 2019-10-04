54c9a89c39d96d92bf39a9add32e29fef6a58f16
Felix Melligan <felixmelligan@hotmail.co.uk>
2019-10-04 15:30:25 -0400

    First push

---
+++ src/test/java/application/AppTest.java
@@ -0,0 +1,12 @@
+package application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void shouldRunTest() {
+        assertEquals(7, 4+3);
+    }
+}
